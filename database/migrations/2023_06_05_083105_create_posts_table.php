c8abd7fff8df6f21f30b28d4e9afa1d224458488
sarmadngi <sarmad.hussain@nextgeni.com>
2023-06-06 04:15:16 +0500

    partition migration

--- database/migrations/2023_06_05_083105_create_posts_table.php
+++ database/migrations/2023_06_05_083105_create_posts_table.php
@@ -14,14 +14,27 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedBigInteger('id');
             $table->string('title')->index();
-            $table->longText('content')->fulltext();
+            $table->longText('content');
             $table->unsignedBigInteger('author_id');
             $table->string('image')->nullable();
             $table->timestamps();
-
+            $table->primary(['id', 'created_at']);
         });
+        Schema::table('posts', function (Blueprint $table) {
+            $table->bigIncrements('id')->change();
+        });
+
+        $partition = "ALTER TABLE posts
+            PARTITION BY RANGE (UNIX_TIMESTAMP(created_at))
+            (
+                PARTITION p1 VALUES LESS THAN (UNIX_TIMESTAMP('2021-01-01')),
+                PARTITION p2 VALUES LESS THAN (UNIX_TIMESTAMP('2022-01-01')),
+                PARTITION p3 VALUES LESS THAN (UNIX_TIMESTAMP('2023-01-01')),
+                PARTITION pmax VALUES LESS THAN MAXVALUE
+            )";
+        \Illuminate\Support\Facades\DB::statement($partition);
     }
 
     /**
